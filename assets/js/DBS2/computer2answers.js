3277a29fae93650baa1dfcfce84bcc1024c92082
EvanSvetina <evanthehedgehog43@gmail.com>
2025-11-14 14:22:01 -0800

    Import some assets from DBS

---
+++ assets/js/DBS2/computer2answers.js
@@ -0,0 +1,13 @@
+const computer2answers = [
+    "3", // option 3 for the first question
+    "7", // option 3 for the second question
+    "Octocat", // option 2 for the third question
+    "git push", // option 1 for the fourth question
+    "Merge Conflicts", // option 4 for the fifth question
+    "Code, Code, Code", // option 2 for the sixth question
+    "C", // option 3 for the seventh question
+    "Channels", // option 1 for the eighth question
+    "Communication"  // option 4 for the ninth question
+];
+
+export default computer2answers;
